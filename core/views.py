f74db6eb77809ff8133688b78c1d66c28b15881d
mmpll <mimy43710@gmail.com>
2023-04-23 18:17:08 +0700

    TT

--- core/views.py
+++ core/views.py
@@ -111,8 +111,7 @@ class HomeView(ListView):
     context_object_name = 'items'
 
 
-# class OrderSummaryView(LoginRequiredMixin, View):
-class OrderSummaryView(View):
+class OrderSummaryView(LoginRequiredMixin, View):
     def get(self, *args, **kwargs):
         try:
             order = Order.objects.get(user=self.request.user, ordered=False)
